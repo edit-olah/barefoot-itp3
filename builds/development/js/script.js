5294e78d09e3826635fce60d702809beac23a4cd
edit-olah <edit.olah.01@gmail.com>
2015-05-12 12:43:25 +0100

    correcting global variable in script file

--- builds/development/js/script.js
+++ builds/development/js/script.js
@@ -2074,8 +2074,11 @@
 
 // defining bftip_timeout function for page nav and hamburger menu icon behaviour, and put it into a global variable:
 
-var timeoutId = function bfitp_timeout ($time){
-    setTimeout(function(){ 
+var pageNavTimeout;
+
+function bfitp_timeout ($time){
+    pageNavTimeout = setTimeout(function(){ 
+	
 	console.log('TIMEOUT - from bfitp_timeout function');
 	// slide up/close page nav:
 	$('#PageNav nav').slideUp(600);
@@ -2089,6 +2092,7 @@ var timeoutId = function bfitp_timeout ($time){
 	console.log($('.pageNavLink').data());
 	
 	console.log('TIMEOUT completed - from bfitp_timeout function');
+	
     }, $time);
 };
 
@@ -2137,17 +2141,12 @@ $( document ).ready(function() {
 	console.log('pageNavLink data is recorded: - from pageNavLink click event');
 	console.log($('.pageNavLink').data());
 	
-	
-	
-	// if Hamburger Menu is active and therefore pageNav is visible (when this pageNavLink li is clicked): 
+	// if Hamburger Menu is active (therefore pageNav is visible) and when this pageNavLink is clicked: 
 		
 	if(($('.cmn-toggle-switch').hasClass('active')) && ($('.pageNavLink').data('clicked') === true)){
-	    
 	    console.log('start conditional behaviour when pageNavLink is clicked and ham icon is active');
-	    timeoutId(2000);
+	    bfitp_timeout(4000);
 	    console.log('TIMEOUT called - from conditional statement');
-
-	    
 	}
 	
 
@@ -2186,20 +2185,23 @@ $( document ).ready(function() {
 	    
 	    if(this.classList.contains("active") === true) {
 		this.classList.remove("active");
+		
+		// slide up/close page nav:
 		$('#PageNav nav').slideUp(600);
 		console.log('HAMburger menu is now INACTIVE');
+		// remove recorded data string against page nav link:
 		$('.pageNavLink').removeData('clicked');
 		console.log('pageNavLink data is removed: - from HAM INACTIVE');
 		console.log($('.pageNavLink').data());
-		clearTimeout(timeoutId);
+		clearTimeout(pageNavTimeout);
 		console.log('timeout CLEARED - from HAM INACTIVE');
-		
 		
 	    } else {
 		this.classList.add("active");
+		
+		// slide down/open page nav:
 		$('#PageNav nav').slideDown(600);
 		console.log('HAMburger menu is now ACTIVE');
-
 	    }
 	  });
 	}
